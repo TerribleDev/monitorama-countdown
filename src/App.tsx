c94df51ec4b4566240a58e1be2d561b35c93d282
Tommy Parnell <tommy@terribledev.io>
2018-03-25 03:19:44 -0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import * as moment from "moment";
+import React = require("react");
+
+const monitorama = moment("20180604", "YYYYMMDD");
+export class App extends React.Component<any, {remaining: string}> {
+    private runningInterval?: number;
+    constructor(props: any) {
+        super(props);
+        this.state = {remaining: this.CalulateTime()};
+        this.runningInterval = setInterval(() => {
+            this.Update();
+        }, 1000);
+    }
+    public componentWillUnmount() {
+        if (this.runningInterval) {
+            clearInterval(this.runningInterval);
+        }
+    }
+    public Update() {
+        this.setState({remaining: this.CalulateTime()});
+    }
+    public CalulateTime() {
+        const now = moment();
+        if (now > monitorama) {
+            return "VICTORYYYYYYYYYYYYY";
+        } else {
+            const duration = moment.duration(monitorama.diff(now));
+            return `${duration.days()} days ${duration.hours()} hours ${duration.minutes()} minutes ${duration.seconds()} seconds`;
+        }
+    }
+    public render() {
+        return(
+            <div>
+                <h4 style={{textAlign: "center", marginTop: "30vh", fontFamily: "Arial, sans-serif", fontWeight: "bold", fontSize: "80px"}}>{this.state.remaining}</h4>
+            </div>
+        );
+    }
+
+}
